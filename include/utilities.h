85719a70b898ef40692e5b6eeec50986a71a12bf
Niklas Anderson <nkanders@gmail.com>
2025-05-18 10:30:41 -0700

    Update cmake config to get build working for sign extension work

--- include/utilities.h
+++ include/utilities.h
@@ -1,9 +1,9 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
-#include <stdint.h> // For fixed-width types like int32_t
+#include <stdint.h>  // For fixed-width types like int32_t
 
-// Sign extend a value 
-int32_t signExtend(uint32_t value, int bits);
+// Sign extend a value
+int32_t signExtend(int16_t bits);
 
-#endif // UTILITIES_H
+#endif  // UTILITIES_H
--- tests/utils_function/utilities.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILITIES_H
-#define UTILITIES_H
-
-#include <cstdint>  // For fixed-width types like int32_t
-
-// Sign extend a value
-int32_t signExtend(int16_t imm);
-
-#endif  // UTILITIES_H
