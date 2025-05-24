20492ef846f338688106e2b0d743c614dcae8c57
reecewayt <reecewayt96@gmail.com>
2025-05-24 12:35:22 -0700

    Revert JR instruction to use register value without shift or pc bits

--- src/functional_simulator.cpp
+++ src/functional_simulator.cpp
@@ -176,9 +176,7 @@ void FunctionalSimulator::execute() {
 
         case mips_lite::opcode::JR:
             // Jump register - use the value in Rs as the new PC
-            // TODO: Instructions for MIPs Lite seem unclear but we should verify this
-            // actual mips instruction uses rs_value << 4 | 4 msb's of PC
-            ex_data->alu_result = (ex_data->pc & 0xF0000000) | (ex_data->rs_value * 4); 
+            ex_data->alu_result = ex_data->rs_value;  // Set PC to value in Rs 
             branch_taken = true;
             break;
 
